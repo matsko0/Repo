f68944ef9bedcf727f4e0e3f6905bcf972e48933
matsko0 <aleksandrov800@gmail.com>
2024-12-04 15:33:42 +0100

    v2.0

--- Homework01/Calculation.js
+++ Homework01/Calculation.js
@@ -1,28 +1,22 @@
 let pricePerOne = 119.95;
-console.log("Price per one unit in $:");
-console.log(pricePerOne);
+console.log("Price per one unit is $:" + pricePerOne);
 
 let NumberOfPhones = 30;
-console.log("Number of phones:");
-console.log(NumberOfPhones);
+console.log("Number of phones: " + NumberOfPhones);
 
 let totalAmount = pricePerOne * NumberOfPhones;
-console.log("Total Amount in $ without tax:");
-console.log(totalAmount);
+console.log("Total Amount in $ without tax: " + totalAmount);
 
 let taxRate = 0.05;
 console.log("Tax rate is 5%");
 
 let taxPerPhone = pricePerOne / taxRate;
-console.log("Tax per one phone: ");  // should result 23,99 (doesnt show the comma symbol between.)
-console.log(taxPerPhone);
+console.log("Tax per one phone: " + taxPerPhone);  // should result 23,99 (doesnt show the comma symbol between.)
 
 let taxAmount = totalAmount * taxRate;
-console.log("taxAmount in tax in $:");
-console.log(taxAmount);
+console.log("taxAmount in tax in $: " + taxAmount);
 
 let totalAmountTax = totalAmount + taxAmount;
-console.log("Totam Amount with tax:");
-console.log(totalAmountTax);
+console.log("Totam Amount with tax: " + totalAmountTax);
 
 
