8aa00466579f511077667df457f36a34b9757c07
matsko0 <aleksandrov800@gmail.com>
2024-12-06 23:17:51 +0100

    HW

---
+++ Homework02/homework02.js
@@ -0,0 +1,34 @@
+/* HOMEWORK
+Write javascript code that will get an input from the user and calculate which Chinese Zodiac a given year is in
+
+Formula for Chinese Zodiac caluclation: (year - 4) % 12. Possible values:
+
+0 - Rat
+1 - Ox
+2 - Tiger
+3 - Rabbit
+4 - Dragon
+5 - Snake
+6 - Horse
+7 - Goat
+8 - Monkey
+9 - Rooster
+10 - Dog
+11 - Pig */
+
+let string = prompt("Please enter your year of birth: ")
+let year = parseInt(string);
+
+let zodiacSigns = [ 
+    "0.Rat", "1.Ox", "2.Tiger", "3.Rabbit", "4.Dragon", "5.Snake", "6.Horse", "7.Goat", "8.Monkey", "9.Rooster", "10.Dog", "11.Pig"
+]
+
+if (isNaN(year)) {
+    alert("Invalid input. Please enter a valid number of your birth date.")
+} else {
+    let zodiacCalc = (year - 4) %12;
+    let zodiac = zodiacSigns[zodiacCalc]
+
+    console.log("Your Chinese sodiac sign is number:" + zodiac);
+    
+}
