f5f2015bfc972b901011fae92a6b833104d0c6a4
matsko0 <aleksandrov800@gmail.com>
2024-12-09 16:08:10 +0100

    fix

--- Homework03/Homework03-2/HomeworkPart2.js
+++ Homework03/Homework03-2/HomeworkPart2.js
@@ -10,13 +10,14 @@ Bonus: Make the same function work for converting dog to human years as well */
 let string = prompt("Please enter your age to convert it to DOG&HUMAN years : ")
 let year = parseInt(string);
 
-function calculateAge(age, toHuman) {
-    if(isNaN(year) || year >= 0) {
+function calculateAge(age) {
+    if(isNaN(year) || year <= 0) {
         alert("Invalid input enter a valid number:");
     } else {
         let humanToDog = age * 7;
         let dogToHuman = age / 7;
-        console.log(`This human age in Dog years would be ${humanToDog} & and the same age to human years ${dogToHuman}`);
+        dogToHuman = dogToHuman.toFixed(2);
+        console.log(`The same amount of year in Human to dog is ${humanToDog} & and the same amount of years human years is ${dogToHuman}`);
         alert(`This age in Dog years would be ${humanToDog} and the same age to human years ${dogToHuman}`);
         
     }
